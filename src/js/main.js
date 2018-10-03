b5102d56ed651fda349d3530c672b00c03ae906e
David Brunnthaler <david@screencode.at>
2018-10-03 14:25:14 +0200

    adding possible tattoo motives to main.js #11

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const TATTOO_MOTIVES = ['Eagle', 'Knife', 'Heart', 'Skull', 'Rainbow', 'Bird', 'Wolf', 'Dragon', 'Dolphin', 'Star', 'Zodiac', 'Biker','I Love Mom', 'Wind Rose', 'Arrow', 'An Astronaut being abducted by an alien'];
+const TATTOO_MOTIVES = ['Eagle', 'Knife', 'Heart', 'Skull', 'Rainbow', 'Bird', 'Wolf', 'Dragon', 'Dolphin', 'Star', 'Zodiac', 'Biker','I Love Mom', 'Wind Rose', 'Arrow', 'An Astronaut being abducted by an alien', 'Compass', 'Butterfly', 'Cross', 'Hexagram', 'Tribal', 'Book', 'Flower', 'Dandelion', 'Wings', 'Snake', 'Fox', 'Joker', 'Salvador Dalí: The Elephants', 'Hourglass', 'Ship', 'Mountains', 'Abstract Punctuation', 'Mandala'];
 
 /**
  * Return a possible tattoo motive.
