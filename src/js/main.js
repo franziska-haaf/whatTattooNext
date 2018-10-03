4fb7dd4375f78f7be4d58590b55a65563080e2ed
Ricardo Ebbers <recl@cin.ufpe.br>
2018-10-02 22:04:55 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const TATTOO_MOTIVES = ['Eagle', 'Knife', 'Heart', 'Skull', 'Rainbow', 'Bird', 'Wolf', 'Dragon', 'Dolphin', 'Star', 'Zodiac', 'Biker','I Love Mom', 'Wind Rose', 'Arrow', 'An Astronaut being abducted by an alien'];
+const TATTOO_MOTIVES = ['Eagle', 'Knife', 'Heart', 'Skull', 'Rainbow', 'Bird', 'Wolf', 'Dragon', 'Dolphin', 'Star', 'Zodiac', 'Biker','I Love Mom', 'Wind Rose', 'Arrow', 'An Astronaut being abducted by an alien', 'An Elephant eaten by a snake, definately not a hat.'];
 
 /**
  * Return a possible tattoo motive.
